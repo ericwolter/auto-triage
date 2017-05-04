caa90d88e7f19376e5d44ce2be62b8c5d9a26707
Zhijian LIU <zhijianliu.cs@gmail.com>
2017-05-04 20:22:43 +0800

    dataset preprocessing

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,10 @@
-wget http://phototriage.cs.princeton.edu/data/train_val.zip
-wget http://phototriage.cs.princeton.edu/data/test.zip
-unzip train_val.zip && unzip test.zip
+mkdir -p images/
+wget http://phototriage.cs.princeton.edu/data/train_val.zip && unzip train_val.zip
+mv train_val/train_val_imgs/* images/
+mv train_val/train_pairlist.txt train.txt
+mv train_val/val_pairlist.txt valid.txt
+rm -rf train_val.zip train_val/
+wget http://phototriage.cs.princeton.edu/data/test.zip && unzip test.zip
+mv test/test_imgs/* images/
+mv test/test_pairlist.txt test.txt
+rm -rf test.zip test/
