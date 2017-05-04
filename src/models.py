0a8c159d217d6e8352df36355b97e15a24324f4f
Zhijian LIU <zhijianliu.cs@gmail.com>
2017-05-04 20:14:56 +0800

    dropout + bias regularizer

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from keras.applications.vgg16 import VGG16
-from keras.layers import Input, Lambda, Dense
+from keras.layers import Input, Lambda, Dense, Dropout
 from keras.regularizers import l2
 from keras.models import Model
 
@@ -8,10 +8,12 @@ def remove_last_layer(model):
   model.outputs = [model.layers[-1].output]
   model.layers[-1].outbound_nodes = []
 
-def add_regularizer(model, kernel_regularizer):
+def add_regularizer(model, kernel_regularizer = l2(), bias_regularizer = l2()):
   for layer in model.layers:
     if hasattr(layer, "kernel_regularizer"):
       layer.kernel_regularizer = kernel_regularizer
+    if hasattr(layer, "bias_regularizer"):
+      layer.bias_regularizer = bias_regularizer
 
 def create_model():
   input_a = Input(shape = (224, 224, 3))
@@ -19,16 +21,16 @@ def create_model():
 
   feature_extractor = VGG16()
   remove_last_layer(feature_extractor)
-  add_regularizer(feature_extractor, l2())
+  add_regularizer(feature_extractor)
 
   feature_a = feature_extractor(input_a)
   feature_b = feature_extractor(input_b)
   delta = Lambda(lambda x: x[0] - x[1], output_shape = lambda s: s[0])([feature_a, feature_b])
 
-  hidden1 = Dense(128, activation = "tanh")(delta)
+  hidden1 = Dropout()(Dense(128, activation = "tanh")(delta))
   hidden2 = Dense(128, activation = "tanh")(hidden1)
   output = Dense(2, activation = "softmax")(hidden2)
 
   model = Model([input_a, input_b], output)
-  add_regularizer(model, l2())
+  add_regularizer(model)
   return model
