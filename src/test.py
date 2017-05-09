99c96152da793068d295186a0359c49a824c913a
Zhijian Liu <zhijianliu.cs@gmail.com>
2017-05-09 14:25:45 +0800

    predict with trained weights

--- src/test.py
+++ src/test.py
@@ -3,15 +3,12 @@ import os, argparse
 from models import create_model
 from data import load_data
 
-from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
-
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--gpu", default = "0")
   parser.add_argument("--exp", default = "default")
   parser.add_argument("--model", default = "vgg16")
   parser.add_argument("--optimizer", default = "sgd")
-  parser.add_argument("--epochs", default = 16, type = int)
   parser.add_argument("--batch", default = 4, type = int)
 
   FLAGS = parser.parse_args()
@@ -22,6 +19,8 @@ if __name__ == "__main__":
   model = create_model(FLAGS)
   print model.summary()
 
+  model.load_weights("../exp/" + FLAGS.exp + "/weights.hdf5")
+
   if FLAGS.optimizer == "sgd":
     from keras.optimizers import SGD
     optimizer = SGD(lr = 0.001, momentum = 0.9)
@@ -31,14 +30,8 @@ if __name__ == "__main__":
   else:
     raise NotImplementedError
 
-  os.system("rm -rf ../exp/" + FLAGS.exp + "/")
-  os.system("mkdir -p ../exp/" + FLAGS.exp + "/")
-
-  callbacks = [
-    ModelCheckpoint("../exp/" + FLAGS.exp + "/weights.hdf5", monitor = "val_acc", save_best_only = True, save_weights_only = True),
-    TensorBoard(log_dir = "../exp/" + FLAGS.exp + "/logs/"),
-    ReduceLROnPlateau(monitor = "val_acc", factor = 0.5, patience = 2)
-  ]
-
   model.compile(optimizer, loss = "binary_crossentropy", metrics = ["accuracy"])
-  model.fit(X["train"], Y["train"], validation_data = [X["valid"], Y["valid"]], epochs = FLAGS.epochs, batch_size = FLAGS.batch, callbacks = callbacks, verbose = 1)
+
+  for set in ["train", "valid"]:
+    loss, acc = model.evaluate(X[set], Y[set], batch_size = FLAGS.batch, verbose = 1)
+    print set, "acc:", acc
