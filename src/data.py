471c868d95b79bc939d7483bf25d3aefe1cdbad5
Zhijian Liu <zhijianliu.cs@gmail.com>
2017-05-09 14:32:42 +0800

    preprocess test data

--- src/data.py
+++ src/data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def load_data(FLAGS):
   X, Y = {}, {}
-  for set in ["train", "valid"]:
+  for set in ["train", "valid", "test"]:
     if os.path.exists("../data/" + set + ".npz"):
       files = np.load(open("../data/" + set + ".npz", "r"))
       X[set], Y[set] = [files["X0"], files["X1"]], files["Y"]
@@ -13,9 +13,6 @@ def load_data(FLAGS):
         for line in tqdm.tqdm(file.readlines(), desc = set):
           elements = line.strip().split()
           prefix, a, b = map(int, elements[:3])
-          score = float(elements[3])
-          if set == "valid" and score >= 0.3 and score <= 0.7:
-            continue
           for i, x in enumerate([a, b]):
             image = cv2.imread("../data/images/{:06d}-{:02d}.JPG".format(prefix, x))
             width, height = image.shape[:2]
@@ -28,10 +25,15 @@ def load_data(FLAGS):
             image = cv2.resize(image, (height, width)).astype(np.float32)
             image = np.pad(image, ((0, 224 - width), (0, 224 - height), (0, 0)), mode = "constant", constant_values = 0)
             X[set][i].append(image / 255.)
-          if score < 0.5:
-            Y[set].append([1, 0])
-          else:
-            Y[set].append([0, 1])
+          if set in ["train", "valid"]:
+            score = float(elements[3])
+            if score < 0.5:
+              Y[set].append([1, 0])
+            else:
+              Y[set].append([0, 1])
+            if set == "valid" and score >= 0.3 and score <= 0.7:
+              X[set].pop()
+              Y[set].pop()
       X[set][0], X[set][1], Y[set] = map(np.array, [X[set][0], X[set][1], Y[set]])
       np.savez(open("../data/" + set + ".npz", "w"), X0 = X[set][0], X1 = X[set][1], Y = Y[set])
   return X, Y
