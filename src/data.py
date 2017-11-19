82e0afb43f07fdd218942073116b773184d18787
Eric Wolter <eric.wolter@gmx.de>
2017-11-19 17:52:12 +0100

    adds forward and backward training data

--- src/data.py
+++ src/data.py
@@ -13,30 +13,28 @@ def load_data(FLAGS):
         for line in tqdm.tqdm(file.readlines(), desc = set):
           elements = line.strip().split()
           prefix, a, b = map(int, elements[:3])
-          for i, x in enumerate([a, b]):
-            path = "../data/images/{:06d}-{:02d}.JPG".format(prefix, x)
-            # image = cv2.imread(path)
-            # width, height = image.shape[:2]
-            # if width < height:
-            #   width = int(224. * width / height)
-            #   height = 224
-            # else:
-            #   height = int(224. * height / width)
-            #   width = 224
-            # image = cv2.resize(image, (height, width)).astype(np.float32)
-            # image = np.pad(image, ((0, 224 - width), (0, 224 - height), (0, 0)), mode = "constant", constant_values = 0)
-            # X[set][i].append(image / 255.)
-            X[set][i].append(path)
+          path1 = "../data/images/{:06d}-{:02d}.JPG".format(prefix, a)
+          path2 = "../data/images/{:06d}-{:02d}.JPG".format(prefix, b)
+
+          # forward comparison
+          X[set][0].append(path1)
+          X[set][1].append(path2)
+
           if set in ["train", "valid"]:
-            score = float(elements[3])
-            if score < 0.5:
-              Y[set].append([1, 0])
-            else:
-              Y[set].append([0, 1])
-            if set == "valid" and score >= 0.3 and score <= 0.7:
-              X[set][0].pop()
-              X[set][1].pop()
-              Y[set].pop()
+              score = float(elements[3])
+              if score < 0.5:
+                Y[set].append([1, 0])
+              else:
+                Y[set].append([0, 1])
+
+              if set == "train":
+                  #backward comparison
+                  X[set][0].append(path2)
+                  X[set][1].append(path1)
+                  if score < 0.5:
+                    Y[set].append([0, 1])
+                  else:
+                    Y[set].append([1, 0])
+
       X[set][0], X[set][1], Y[set] = map(np.array, [X[set][0], X[set][1], Y[set]])
-      #np.savez(open("../data/" + set + ".npz", "w"), X0 = X[set][0], X1 = X[set][1], Y = Y[set])
   return X, Y
